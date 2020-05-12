68a68ab09cd4e580d4d169bf433bb45d1d736d51
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-05-11 23:52:38 -0600

    Working timeline.

---
+++ events.js
@@ -0,0 +1,13 @@
+const HUNDRED = 100;
+const THOUSAND = 1000;
+const MILLION = THOUSAND * THOUSAND;
+const BILLION = THOUSAND * MILLION;
+
+let timeline_events = [
+  { age: 70 * THOUSAND, title: "sample 1" },
+  { age: 0, title: "sample 2" },
+  { age: 7 * THOUSAND, title: "sample 3" },
+  { age: BILLION, title: "sample 5" },
+  { age: 4.54 * BILLION, title: "earth formed" },
+  { age: MILLION, title: "Really super-duper unnecessarily long title" },
+];
